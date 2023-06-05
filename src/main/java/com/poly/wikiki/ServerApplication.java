6519bc170d5bbe7088afb34edba570407c2fed5d
sonptph19600 <sonptph19600@fpt.edu.vn>
2023-06-06 01:44:11 +0700

    first commit

---
+++ src/main/java/com/poly/wikiki/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.poly.wikiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/poly/wikiki/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poly.wikiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
